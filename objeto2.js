2413fcc49c9395d109607489ea844f3f207e832d
davidweb182 <davidjoseuh18@gmail.com>
2020-08-05 21:02:46 -0500

    update

--- objeto2.js
+++ objeto2.js
@@ -1,33 +1,3 @@
-if (typeof Object.assign !== 'function') {
-  // Must be writable: true, enumerable: false, configurable: true
-  Object.defineProperty(Object, "assign", {
-    value: function assign(target, varArgs) { // .length of function is 2
-      'use strict';
-      if (target === null || target === undefined) {
-        throw new TypeError('Cannot convert undefined or null to object');
-      }
-
-      var to = Object(target);
-
-      for (var index = 1; index < arguments.length; index++) {
-        var nextSource = arguments[index];
-
-        if (nextSource !== null && nextSource !== undefined) { 
-          for (var nextKey in nextSource) {
-            // Avoid bugs when hasOwnProperty is shadowed
-            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-              to[nextKey] = nextSource[nextKey];
-            }
-          }
-        }
-      }
-      return to;
-    },
-    writable: true,
-    configurable: true
-  });
-}
-
 const pedido = {
   nombre: "",
   apellido:"",
@@ -46,7 +16,7 @@ for (let index = 0; index <persona; index++) {
   pedido.nombre = prompt(`Dime solo Tu nombre porfavor, numero ${index}`);
   pedido.apellido = prompt(`Dime tu primer apellido porfavor, numero ${index}`);
   pedido.edad = parseInt(prompt(`Dime tu edad porfavor, numero ${index}`));
-  pedidos.push(Object.assing({}, pedido));
+  pedidos.push(Object.assign({}, pedido));
 }
 
 for (let i = 0; i < pedidos.length; i++) {
